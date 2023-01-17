4b38abe8ba7af7050314ced1534082695232b2b5
Legazole <LegazoleNFT@gmail.com>
2023-01-17 10:39:34 +0100

    fixed incident

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,8 +11,7 @@ require("hardhat-deploy")
  */
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const GOERLI_RPC_URL =
-    process.env.GOERLI_RPC_URL
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
